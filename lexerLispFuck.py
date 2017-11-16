117515fe02cc66a07b29e1bfb68e1abc4b82f59e
Adrianne Alves <driah_as@hotmail.com>
2017-11-16 10:49:36 -0300

    Changing lexer structure

--- lexerLispFuck.py
+++ lexerLispFuck.py
@@ -1,16 +1,41 @@
-import tokenize
+import re
+from collections import namedtuple
 
-st = "(do read inc (loop print dec))"
 
-def make_line_getter(code):
-    lines = code.splitlines()    
-    def getter():
-        if lines:
-            line = lines.pop(0)            
-            return line.encode('utf-8')
-        else:
-            return b''
-    return getter
+Token = namedtuple(
+    'Token', 
+    ['type', 'data', 'lineno']
+)
 
-for tok in tokenize.tokenize(make_line_getter(st)):
-    print(tok)
+regex_map = [
+    ('LPAR', r'\('),
+    ('RPAR', r'\)'),
+    ('OP', r'inc|dec|print|read|loop|right|left|'),
+    ('SPACE', r'\s+'),
+]
+
+test1 = '(do read inc (loop print dec))'
+
+template = r'(?P<{name}>{regex})'
+REGEX_ALL = '|'.join(
+    template.format(name=name, regex=regex)
+    for (name, regex) in regex_map
+)
+re_all = re.compile(REGEX_ALL)
+
+source = '\n'.join([test1])
+
+def lexer(source):
+    lineno = 1
+    for m in re_all.finditer(source):
+        type_ = m.lastgroup
+        if type_ == 'SPACE':
+            continue
+        elif type_ == 'NEWLINE':
+            lineno += 1
+            continue 
+        i, j = m.span()
+        data = m.string[i:j]        
+        yield Token(type_, data, lineno)
+
+print (list(lexer(source)))
