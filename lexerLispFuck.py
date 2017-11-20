31c0694bb92f55fb7eb50bee112b737fbadea0b4
Adrianne Alves <driah_as@hotmail.com>
2017-11-20 20:02:33 -0300

    Adding read of file

--- lexerLispFuck.py
+++ lexerLispFuck.py
@@ -1,32 +1,37 @@
+import click
 import re
 from collections import namedtuple
 
+@click.command()
+@click.argument('lispf_ck',type=click.File('r'))
 
-Token = namedtuple(
+# O comando click usado pra obter argumentos na linha de comando
+
+def tokenize(lispf_ck):
+    source = lispf_ck.read()
+    print(list(lexer(source)))
+
+def lexer(source):
+    Token = namedtuple(
     'Token', 
     ['type', 'data', 'lineno']
-)
+    )
 
-regex_map = [
+    regex_map = [
     ('LPAR', r'\('),
     ('RPAR', r'\)'),
     ('NUMBER', r'[0-9]+(\.[0-9]*)?'),
     ('OP', r'inc|dec|print|read|loop|right|left|add|def'),
     ('SPACE', r'\s+'),
-]
+    ]
 
-test1 = '(do read inc (loop print dec))'
-
-template = r'(?P<{name}>{regex})'
-REGEX_ALL = '|'.join(
+    template = r'(?P<{name}>{regex})'
+    REGEX_ALL = '|'.join(
     template.format(name=name, regex=regex)
     for (name, regex) in regex_map
-)
-re_all = re.compile(REGEX_ALL)
+    )
+    re_all = re.compile(REGEX_ALL)
 
-source = '\n'.join([test1])
-
-def lexer(source):
     lineno = 1
     for m in re_all.finditer(source):
         type_ = m.lastgroup
@@ -36,7 +41,11 @@ def lexer(source):
             lineno += 1
             continue 
         i, j = m.span()
-        data = m.string[i:j]        
+        data = m.string[i:j]
+        
         yield Token(type_, data, lineno)
 
-print (list(lexer(source)))
+
+if __name__ == "__main__":
+    tokenize()
+    
