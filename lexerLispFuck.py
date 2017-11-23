cc460fc89fe3107552bf83aa169a824ca89955e3
Adrianne Alves <driah_as@hotmail.com>
2017-11-22 21:57:45 -0300

    Fixing tree generator

--- lexerLispFuck.py
+++ lexerLispFuck.py
@@ -4,42 +4,39 @@ import click
 @click.command()
 @click.argument('source', type=click.File('r'))
 
-def make_tree(source):    
+def get_tree(source):
 
-    lexer = ox.make_lexer([
+    lexer = ox.make_lexer([        
+        ('NUMBER', r'\d+'),
+        ('NAME', r'[-a-zA-Z]+'),
+        ('COMMENT', r';.*'),
+        ('NEWLINE', r'\n'),
+        ('SPACE', r'\s+'),
         ('RIGHT', r'right'),
         ('LEFT', r'left'),
         ('INC', r'inc'),
         ('DEC', r'dec'),
+        ('ADD',r'add'),
+        ('SUB',r'sub'),
         ('PRINT', r'print'),
         ('READ', r'read'),
         ('DO',r'do'),
-        ('ADD',r'add'),
-        ('SUB',r'sub'),
         ('LOOP', r'loop'),
         ('DEF', r'def'),
-        ('NUMBER', r'\d+'),
-        ('PARENTESE_A', r'\('),
-        ('PARENTESE_F', r'\)'),
-        ('NAME', r'[-a-zA-Z]+'),
-        ('COMMENT', r';.*'),
-        ('NEWLINE', r'\n'),
-        ('SPACE', r'\s+')
+        ('PARENTHESIS_B', r'\('),
+        ('PARENTHESIS_A', r'\)')        
     ])
 
-    tokens = ['RIGHT', 'LEFT', 'INC', 'DEC', 'SUB', 'ADD', 'NUMBER','PRINT', 'LOOP',
-                'READ','DEF','PARENTESE_F','PARENTESE_A','DO','NAME']
+    tokens = ['NUMBER','INC', 'DEC','SUB', 'ADD','RIGHT', 'LEFT','PRINT','DO','NAME','LOOP',
+                'READ','DEF','PARENTHESIS_A','PARENTHESIS_B']
+
+    op = lambda op: (op)
+    operator = lambda type_op: (type_op)
     
-    program = source.read()
-
-    operator = lambda type_op: type_op
-
-    op = lambda op: op
-    opr = lambda op, num: (op, num)
 
     parser = ox.make_parser([
-        ('program : PARENTESE_A expr PARENTESE_F', lambda x,y,z: y),
-        ('program : PARENTESE_A PARENTESE_F', lambda x,y: '()'),
+        ('program : PARENTHESIS_B expr PARENTHESIS_A', lambda x,y,z: y),
+        ('program : PARENTHESIS_B PARENTHESIS_A', lambda x,y: '()'),
         ('expr : operator expr', lambda x,y: (x,) + y),
         ('expr : operator', lambda x: (x,)),
         ('operator : program', op),
@@ -58,8 +55,14 @@ def make_tree(source):
         ('operator : NUMBER', operator),
     ], tokens)
 
-    tree = parser(tokens)
+    program = source.read()
 
-    
+    tokens = lexer(program)
+
+    parserTokens = [token for token in tokens if token.type != 'COMMENT' and token.type != 'SPACE']
+
+    tree = parser(parserTokens)
+    print(tree)
+
 if __name__ == '__main__':
-    make_tree()
+    get_tree()
